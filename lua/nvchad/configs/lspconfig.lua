46b15ef1b9d10a83ab7df26b14f474d15c01e770
siduck <siduck@tutanota.com>
2025-04-08 18:49:35 +0530

    rm: dont remap inbuilt mappings of v0.11 nvim

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -9,8 +9,6 @@ M.on_attach = function(_, bufnr)
 
   map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
   map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
-  map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
-  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
   map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
   map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
 
@@ -20,9 +18,6 @@ M.on_attach = function(_, bufnr)
 
   map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
   map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
-
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
-  map("n", "gr", vim.lsp.buf.references, opts "Show references")
 end
 
 -- disable semanticTokens
