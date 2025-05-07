d4e5b8a10f01c06e18d2a8bb66761a3596562a67
siduck <siduck@tutanota.com>
2025-05-07 21:42:12 +0530

    update mason.nvim repo owner

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -84,7 +84,7 @@ return {
 
   -- lsp stuff
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     cmd = { "Mason", "MasonInstall", "MasonUpdate" },
     opts = function()
       return require "nvchad.configs.mason"
