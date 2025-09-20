f107fabe11ac8013dc3435ecd5382bee872b1584
siduck <siduck@tutanota.com>
2025-09-20 06:11:18 +0530

    fix(mappings): disable when tabufline is disabled
    https://github.com/NvChad/ui/pull/514

--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -29,19 +29,21 @@ end, { desc = "general format file" })
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
 
 -- tabufline
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+if require("nvconfig").ui.tabufline.enabled then
+  map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
 
-map("n", "<tab>", function()
-  require("nvchad.tabufline").next()
-end, { desc = "buffer goto next" })
+  map("n", "<tab>", function()
+    require("nvchad.tabufline").next()
+  end, { desc = "buffer goto next" })
 
-map("n", "<S-tab>", function()
-  require("nvchad.tabufline").prev()
-end, { desc = "buffer goto prev" })
+  map("n", "<S-tab>", function()
+    require("nvchad.tabufline").prev()
+  end, { desc = "buffer goto prev" })
 
-map("n", "<leader>x", function()
-  require("nvchad.tabufline").close_buffer()
-end, { desc = "buffer close" })
+  map("n", "<leader>x", function()
+    require("nvchad.tabufline").close_buffer()
+  end, { desc = "buffer close" })
+end
 
 -- Comment
 map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
